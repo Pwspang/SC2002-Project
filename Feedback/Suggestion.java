1d05eaec051ddac45bb4a4b6b31518f9dc3e572e
Charmain <charmainnang@gmail.com>
2023-11-18 01:15:22 +0800

    Included constructors, getters and setters

--- Feedback/Suggestion.java
+++ Feedback/Suggestion.java
@@ -1,6 +1,20 @@
 package Feedback;
+import java.util.Objects;
 
 public class Suggestion extends Feedback {
-    
+    private boolean isApproved;
 
+    public Suggestion(int feedbackID, String userID, String campID, String content, boolean isApproved) {
+        super(feedbackID, userID, campID, content);
+        this.isApproved = false;
+    }
+
+    public boolean isApproved() {
+        return this.isApproved;
+    }
+
+    public void setIsApproved(boolean isApproved) {
+        this.isApproved = isApproved;
+    }   
+    
 }
