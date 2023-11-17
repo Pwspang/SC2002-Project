22bd10a9576ce7b8810581c546dac610163881d3
revold <retardedvoldemort2@gmail.com>
2023-11-17 18:50:49 +0800

    camp

---
+++ campPackage/App.java
@@ -0,0 +1,12 @@
+package camppackage;
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Camp mycamp = new Camp("CAMP1234", new CampInformation());
+        
+        CampInformation a = mycamp.getCampInfo();
+        System.out.println(a.getSlotsSummary());
+    }
+}
---
+++ campPackage/AttendeeSlots.java
@@ -0,0 +1,28 @@
+package camppackage;
+import java.util.*;
+
+public class AttendeeSlots extends Slots {
+    protected static ArrayList<String> stuWithdrawn;
+
+    /**
+	 * Constructs a Camp object based on CampInformation.
+	 * @param id Unique Camp name
+	 * @param CampInformation Information needed to construct Camp
+	 */
+    public AttendeeSlots(int totalSlots) {
+        super(totalSlots);
+    }
+
+    public void register(String studentID) {
+        if (!stuWithdrawn.contains(studentID)) throw new RuntimeException("Cannot rejoin after withdrawal.");
+        super.register(studentID);
+    }
+
+    public void withdraw(String studentID) {
+        if (!stuRegistered.contains(studentID)) throw new RuntimeException("Not registered.");
+        stuRegistered.remove(studentID);
+        stuWithdrawn.add(studentID);
+        this.addCount(-1);
+    }
+
+}
