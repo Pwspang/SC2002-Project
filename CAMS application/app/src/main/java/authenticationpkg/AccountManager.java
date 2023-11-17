b6f1d07bb3e54055f45f65168ec7dbe6c91aaf49
chesbulla <chesterchua722@gmail.com>
2023-11-18 00:54:14 +0800

    hope it works

--- CAMS application/app/src/main/java/authenticationpkg/AccountManager.java
+++ CAMS application/app/src/main/java/authenticationpkg/AccountManager.java
@@ -1,35 +1,54 @@
 package authenticationpkg;
+
 import java.io.Serializable;
 import java.util.HashMap;
 
-public class AccountManager implements Serializable{
-    private Map<String,AuthUser> accountDict;
-    private static AccountManager accountManager;
+public class AccountManager implements Serializable {
+	private HashMap<String, AuthUser> accountDict;
+	private static final AccountManager accountManager = new AccountManager();
+	private static final String filename = "AccountManager.dat";
 
-    
-    public static void writeSerializedObject(String filename, Map<String, AuthUser> accountDict){
-        FileOutputStream fos = null;
-        ObjectOutputStream out = null;
-        try {
-            fos = new FileOutputStream(filename);
-            out = new ObjectOutputStream(fos);
-            out.writeObject(accountDict);
-            out.close();
-            System.out.println("Object Persisted");
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
+	private AccountManager() {
+		accountDict = readSerializedObject(filename);
 
-    public static void 
+	}
 
+	public static void writeSerializedObject() {
+		FileOutputStream fos = null;
+		ObjectOutputStream out = null;
+		try {
+			fos = new FileOutputStream(filename);
+			out = new ObjectOutputStream(fos);
+			out.writeObject(accountDict);
+			out.close();
+			System.out.println("Object Persisted");
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
 
+	public static HashMap<String, AuthUser> readSerializedObject() {
+		Map<String, AuthUser> pDetails = null;
+		FileInputStream fis = null;
+		ObjectInputStream in = null;
+		try {
+			fis = new FileInputStream(filename);
+			in = new ObjectInputStream(fis);
+			pDetails = (HashMap<String, AuthUser>) in.readObject();
+			in.close();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} catch (ClassNotFoundException ex) {
+			ex.printStackTrace();
+		}
+		// print out the size
+		// System.out.println(" Details Size: " + pDetails.size());
+		// System.out.println();
+		return pDetails;
+	}
+
+	public static AccountManager getInstance() {
+		return accountManager;
+	}
 
 }
-
-
-
-
-
-
-
