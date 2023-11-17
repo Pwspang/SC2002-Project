c03c785bbadb69383622b72eef37424950495dd9
chesbulla <chesterchua722@gmail.com>
2023-11-18 04:26:37 +0800

    fixed feedback pkg folder

---
+++ CAMS application/app/src/main/java/feedbackpkg/Suggestion.java
@@ -0,0 +1,19 @@
+package feedbackpkg;
+
+public class Suggestion extends Feedback {
+    private boolean isApproved;
+
+    public Suggestion(int feedbackID, String userID, String campID, String content, boolean isApproved) {
+        super(feedbackID, userID, campID, content);
+        this.isApproved = false;
+    }
+
+    public boolean isApproved() {
+        return this.isApproved;
+    }
+
+    public void setIsApproved(boolean isApproved) {
+        this.isApproved = isApproved;
+    }
+
+}
---
+++ CAMS application/app/src/main/java/feedbackpkg/iFeedbackCC.java
@@ -0,0 +1,17 @@
+package feedbackpkg;
+
+import java.util.ArrayList;
+
+public interface iFeedbackCC extends iFeedbackStudent {
+    public ArrayList<String> getCampEnquiries(String userID, ArrayList<String> regCampList);
+
+    public void replyEnquiry(int feedbackID, ArrayList<String> regCampList, String campID, String replyContent);
+
+    public ArrayList<String> getSuggestions(String CampCommID);
+
+    public void editSuggestion(int feedbackID, String CampCommID, String newContent);
+
+    public void deleteSuggestion(int feedbackID, String CampCommID);
+
+    public void submitSuggestion(String CampCommID, String campID, String content);
+}
