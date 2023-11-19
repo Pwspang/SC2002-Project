66f0220069240eea9014080855240ff2e75f81fa
chesbulla <chesterchua722@gmail.com>
2023-11-20 02:57:39 +0800

    added more filters as per assignment requirement

--- CAMS application/app/src/main/java/reportpkg/ReportFilterCamp.java
+++ CAMS application/app/src/main/java/reportpkg/ReportFilterCamp.java
@@ -4,19 +4,19 @@ import camppkg.*;
 
 public abstract class ReportFilterCamp {
     private String filename;
-    private String campID;
+    private String ID;
 
-    public ReportFilterCamp(String campID, String filename) {
+    public ReportFilterCamp(String ID, String filename) {
         this.filename = filename;
-        this.campID = campID;
+        this.ID = ID;
     }
 
     public String getFileName() {
         return filename;
     }
 
-    public String getCampID() {
-        return campID;
+    public String getID() {
+        return ID;
 
     }
 
--- CAMS application/app/src/main/java/reportpkg/test.java
+++
@@ -1,9 +0,0 @@
-package reportpkg;
-
-public class test {
-    public static void main(String[] args) {
-
-        System.out.println("Hello");
-    }
-
-}
