18ee48af5e7a8c04f1acdc9e967484624f28cc72
HASBULLA <chesterchua722@gmail.com>
2023-11-25 05:10:33 +0800

    Update SearchFilter.java

--- CAMS application/app/src/main/java/searchfilterpkg/SearchFilter.java
+++ CAMS application/app/src/main/java/searchfilterpkg/SearchFilter.java
@@ -14,7 +14,7 @@ interface SearchFilter {
      * @param filterOption Data used to filter the list (e.g. The name of
      *                     location or the date of camp).
      * @param campList     The list of camps that will be filtered
-     * @return
+     * @return The filtered list of campIDs.
      */
     ArrayList<String> filter(String filterOption, ArrayList<String> campList);
 
