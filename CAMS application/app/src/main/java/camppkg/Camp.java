0bd4fc0a8ddf81db1d82423c017329d6598acb3a
revold <retardedvoldemort2@gmail.com>
2023-11-26 13:36:15 +0800

    Remaining Javadocs

--- CAMS application/app/src/main/java/camppkg/Camp.java
+++ CAMS application/app/src/main/java/camppkg/Camp.java
@@ -3,34 +3,71 @@ package camppkg;
 import java.io.Serializable;
 import java.util.*;
 
+/**
+ * Represents a camp in the system.
+ */
 public class Camp implements Serializable {
+    /**
+     * The unique ID for this camp.
+     */
     private String id;
+    /**
+     * The visibility of the camp. Auto set to False when Camp is first created.
+     */
     private Boolean visibility;
+    /**
+     * The CampInformation object which students can view.
+     */
     private CampInformation campinfo;
+    /**
+     * An ArrayList of students withdrawn from this camp.
+     */
     private ArrayList<String> stuWithdrawn = new ArrayList<>();
 
+    /**
+     * Constructs a new Feedback with the specified user ID, camp ID, and content.
+     * The feedback ID is automatically generated and incremented for each new Feedback.
+     *
+     * @param campID The ID of the camp the feedback is associated with.
+     * @param campinfo The CampInformation wrapper object for the supplied information.
+     */
     public Camp(String campID, CampInformation campinfo) {
         this.id = campID;
         this.visibility = true;
         this.campinfo = campinfo;
     }
 
+    /**
+     * @return ID of the Camp object.
+     */
     public String getID() {
         return this.id;
     }
 
+    /**
+     * @return The CampInformation wrapper object for the supplied information.
+     */
     public CampInformation getCampInfo() {
         return this.campinfo;
     }
 
+    /**
+     * @return The visibility of this camp. If the camp is open for students to view.
+     */
     public Boolean getVisibility() {
         return this.visibility;
     }
 
+    /**
+     * @return ArrayList of students who previously withdrawn from this Camp.
+     */
     public ArrayList<String> getStuWithdrawn() {
         return stuWithdrawn;
     }
 
+    /**
+     * @return ArrayList of students who are registered to this Camp.
+     */
     public ArrayList<String> getRegisteredStudents() {
         ArrayList<String> result = new ArrayList<>();
         HashMap<String, Slots> allSlots = campinfo.getAllSlots();
@@ -41,6 +78,9 @@ public class Camp implements Serializable {
         return result;
     }
 
+    /**
+     * @return HashMap of registered students and their associated role.
+     */
     public HashMap<String, String> getRegisteredStudentRoles() {
         HashMap<String, String> result = new HashMap<>();
         HashMap<String, Slots> allSlots = campinfo.getAllSlots();
@@ -53,6 +93,10 @@ public class Camp implements Serializable {
         return result;
     }
 
+    /**
+     * Cannot set visibility to False if there are already students registered to the Camp.
+     * @param visibility sets the visibility of this camp
+     */
     public void setVisibility(Boolean visibility) {
         ArrayList<String> stuRegistered = getRegisteredStudents();
         if (!stuRegistered.isEmpty() && visibility == false) {
@@ -61,6 +105,11 @@ public class Camp implements Serializable {
         this.visibility = visibility;
     }
 
+    /**
+     * Register function for this camp.
+     * @param studentID ID of the student registering
+     * @param roleID ID of the desired role
+     */
     public void register(String studentID, String roleID) {
         ArrayList<String> stuRegistered = getRegisteredStudents();
         if (stuRegistered.contains(studentID)) throw new RuntimeException("Already registered.");
@@ -69,6 +118,10 @@ public class Camp implements Serializable {
         mySlots.register(studentID);
     }
 
+    /**
+     * Withdraw function for this camp.
+     * @param studentID ID of the withdrawing student
+     */
     public void withdraw(String studentID) {
         HashMap<String, Slots> allSlots = campinfo.getAllSlots();
         for (Slots mySlots : allSlots.values()) {
@@ -80,6 +133,11 @@ public class Camp implements Serializable {
         }
     }
 
+    /**
+     * Returns a string representation of the Camp.
+     *
+     * @return a string representation of the Camp
+     */
     @Override
     public String toString(){
         return campinfo.toString();
--- CAMS application/app/src/main/java/camppkg/iCampStudent.java
+++ CAMS application/app/src/main/java/camppkg/iCampStudent.java
@@ -4,16 +4,55 @@ import authenticationpkg.Faculty;
 
 public interface iCampStudent {
     
+    /**
+	 * Get the list of camps visible for the student of a certain faculty.
+	 * 
+	 * @param faculty the faculty of the viewing Student
+     * @return ArrayList of campID visible to students of the faculty
+	 */
     ArrayList<String> getVisibleCampList(Faculty faculty);
     
+    /**
+	 * Get the list of registered camps for a particular student regardless of role.
+	 * 
+	 * @param studentID ID of the Student user.
+     * @return ArrayList of campID of the registered camps.
+	 */
     ArrayList<String> getRegisteredCampList(String studentID, String roleID);
 
+    /**
+	 * Get the camp information of a cmap.
+	 * 
+	 * @param campID ID of the target camp.
+     * @return CampInformation object for the target camp.
+	 */
     CampInformation getCampInfo(String campID);
 
+    /**
+	 * Get the Camp object associated with the campID.
+	 * 
+	 * @param campID ID of the target camp.
+     * @return Camp object.
+	 */
     Camp getCamp(String campID);
 
+    /**
+	 * Allow a student to register for a role in a camp.
+     * Student cannot register if they have already registered or if registeration period is over.
+     * Or if the camp clashes with one of the other camps they have already registered for.
+	 * 
+	 * @param campID ID of the target camp
+     * @param studentID ID of the student registering
+     * @param roleID ID of the desired role
+	 */
     void register(String campID, String studentID, String roleID);
 
+    /**
+	 * Allow a student to withdraw from a camp.
+	 * 
+	 * @param campID ID of the target camp
+     * @param studentID ID of the student withdrawing
+	 */
     void withdraw(String campID, String studentID);
 
 }
